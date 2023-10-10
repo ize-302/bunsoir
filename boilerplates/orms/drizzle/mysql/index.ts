b7d841934574450555b5598c4a1fd8c7e20bd43c
ize-302 <adavizeozorku@gmail.com>
2023-10-10 20:50:52 +0100

    :sparkles: Add drizzle > mysql support
    add drizzle > mysql support

--- boilerplates/orms/drizzle/mysql/index.ts
+++ boilerplates/orms/drizzle/mysql/index.ts
@@ -2,9 +2,9 @@ import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 
 const connection = await mysql.createConnection({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST as string,
   user: process.env.DB_USER,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_DATABASE as string,
 });
 
 export const db = drizzle(connection);
