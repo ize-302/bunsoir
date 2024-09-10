64af0ddd2d714254e1fe5b32c87548a706d56b89
ize-302 <adavize302@gmail.com>
2024-09-10 15:43:27 +0100

    fix: fix build issue relating to shelljs

--- build.mjs
+++ build.mjs
@@ -1,9 +1,7 @@
-// import dts from 'bun-plugin-dts'
-
 await Bun.build({
   target: 'node',
   entrypoints: ['./src/index.ts'],
   outdir: './dist',
-  minify: false,
-  // plugins: [dts()]
+  minify: true,
+  external: ['shelljs']
 })
